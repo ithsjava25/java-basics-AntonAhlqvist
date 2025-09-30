62923aae7e29bf0d5bd2288495f567ed511829d1
Anton Ahlqvist <anton.ahlqvist@iths.se>
2025-09-30 21:30:18 +0200

    Add ArgsResult class and parseArgs in Main

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,7 +2,72 @@ package com.example;
 
 public class Main {
 
+    public static class ArgsResult {
+        private String zone;
+        private String date;
+        private boolean sorted;
+        private String charging;
+
+        public String getZone() {
+            return zone;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public boolean isSorted() {
+            return sorted;
+        }
+
+        public String getCharging() {
+            return charging;
+        }
+
+        public void setZone(String zone) {
+            this.zone = zone;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public void setSorted(boolean sorted) {
+            this.sorted = sorted;
+        }
+
+        public void setCharging(String charging) {
+            this.charging = charging;
+        }
+
+        public void printInfo() {
+            System.out.println("Print zone: " + zone);
+            System.out.println("Print date: " + date);
+            System.out.println("Print sorted: " + sorted);
+            System.out.println("Print charging: " + charging);
+        }
+    }
+
+    private static ArgsResult parseArgs(String[] args) {
+        ArgsResult result = new ArgsResult();
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[0].equals("--zone")) {
+                result.setZone(args[i + 1]);
+            } else if (args[0].equals("--date")) {
+                result.setDate(args[i + 1]);
+            } else if (args[0].equals("--sort")) {
+                result.setSorted(true);
+            } else if (args[0].equals("--charging")); {
+                result.setCharging(args[i + 1]);
+            }
+        }
+            return result;
+    }
+
     public static void main(String[] args) {
 
+        ArgsResult parsed = parseArgs(args);
+        parsed.printInfo();
     }
 }
